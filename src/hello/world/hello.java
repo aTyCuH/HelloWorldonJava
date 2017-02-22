601f36a1b66ea496b3d5ca5b9b575bdc63acedbf
aTyCuH <palienko1989@bigmir.net>
2017-02-22 21:05:56 +0200

    Fixed some bugs

--- src/hello/world/hello.java
+++ src/hello/world/hello.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class hello {
 
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
 
-            System.out.println("Please enter your name:");
-            String name = input.nextLine();
-            System.out.format("Hello, %n", name);
+        Scanner input = new Scanner(System.in);
 
-            System.out.println("Please enter your age:");
-            int age = input.nextInt();
-            System.out.format("Oh , you`re !  and you old %n", age);
+        System.out.println("Please, enter your name:");
+        String name = input.nextLine();
+        System.out.format("Hello, %s!%n", name);
 
-            System.out.println("Please enter your age:");
-            float weight = input.nextFloat();
-            System.out.format("Oh , you`re fat  and you old", weight);
+        System.out.println("Please, enter your age:");
+        int age = input.nextInt();
+        System.out.format("Oh, you're %d! And you're an old man!%n", age);
 
+        System.out.println("Please, enter your weight:");
+        float weight = input.nextFloat();
+        System.out.format("Oh, you're fat! Your weight is %1.2f%n", weight);
 
-        }
-
+        input.close();
+    }
 }
